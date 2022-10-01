bdf6cec843896ee622f8efc58dcd116b0f590b0b
vladislavaSim <simonova.vladislava@gmail.com>
2022-10-01 22:24:10 +0200

    notfound page created

---
+++ src/components/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const NotFound = () => {
+    return (
+        <>
+            <h2 style={{color: "red"}}>
+                Ooops! The page is not found :(
+                    </h2>
+        </>
+    )
+}
